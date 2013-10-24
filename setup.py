6352d349449cf4eb19d5b2983551f60da675ee7b
Josh West <joshwest@theatlantic.com>
2013-10-23 20:53:34 -0400

    Increment version number to 1.0.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='django-contact-form',
-      version='0.3',
+      version=__import__('contact_form').__version__,
       description='Generic contact-form application for Django',
       author='James Bennett',
       author_email='james@b-list.org',
